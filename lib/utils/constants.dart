a6d1627071929d65a1c9a66d7080f5b712d582fa
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 18:13:19 +0530

    Create constants.dart

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String appName = "Us";
+
+  static const String users = "users";
+  static const String posts = "posts";
+  static const String chats = "messages";
+  static const String relationships = "relationships";
+  static const String stories = "stories";
+}
