bde7920457accadd516ad15c82e18ce40ed10a46
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 19:06:32 +0530

    Delete lib/models/relationship_provider.dart

--- lib/models/relationship_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../services/relationship_service.dart';
-
-final relationshipServiceProvider = Provider((ref) {
-  return RelationshipService();
-});
