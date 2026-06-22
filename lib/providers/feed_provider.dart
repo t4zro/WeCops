7321c785359d24521e6e7e5d837d63d107b1bfb2
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 19:10:10 +0530

    Update feed_provider.dart

--- lib/providers/feed_provider.dart
+++ lib/providers/feed_provider.dart
@@ -1,12 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../models/post_model.dart';
 import '../services/feed_service.dart';
 
-final feedServiceProvider = Provider<FeedService>((ref) {
+final feedServiceProvider = Provider((ref) {
   return FeedService();
-});
-
-final feedProvider =
-    StreamProvider.family<List<Post>, String>((ref, relationshipId) {
-  return ref.watch(feedServiceProvider).getFeed(relationshipId);
 });
