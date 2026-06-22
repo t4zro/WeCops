76e1fb3935191700a32629d1db327afb67e89b70
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 19:16:45 +0530

    Create custom_button.dart

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final bool loading;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.loading = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 50,
+      child: ElevatedButton(
+        onPressed: loading ? null : onPressed,
+        child: loading
+            ? const CircularProgressIndicator(color: Colors.white)
+            : Text(text),
+      ),
+    );
+  }
+}
