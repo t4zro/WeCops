a9cee3ff490b12e07c7f8ba75a621d6cde81ebf2
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 18:14:19 +0530

    Add AppRoutes class for application navigation

---
+++ lib/utils/routes.dart
@@ -0,0 +1,10 @@
+class AppRoutes {
+  static const splash = "/";
+  static const login = "/login";
+  static const signup = "/signup";
+  static const home = "/home";
+  static const chat = "/chat";
+  static const profile = "/profile";
+  static const camera = "/camera";
+  static const settings = "/settings";
+}
