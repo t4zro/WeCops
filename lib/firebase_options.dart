e75b1d4e1f410a711a4e136ba30dc721bf87e799
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 19:32:15 +0530

    Update firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,1 +1,58 @@
-. 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      return web;
+    }
+
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        return ios;
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
+  }
+
+  // ======================================================
+  // WEB
+  // ======================================================
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: "YOUR_WEB_API_KEY",
+    appId: "YOUR_WEB_APP_ID",
+    messagingSenderId: "YOUR_SENDER_ID",
+    projectId: "YOUR_PROJECT_ID",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+  );
+
+  // ======================================================
+  // ANDROID
+  // ======================================================
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: "YOUR_ANDROID_API_KEY",
+    appId: "YOUR_ANDROID_APP_ID",
+    messagingSenderId: "YOUR_SENDER_ID",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+  );
+
+  // ======================================================
+  // IOS
+  // ======================================================
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: "YOUR_IOS_API_KEY",
+    appId: "YOUR_IOS_APP_ID",
+    messagingSenderId: "YOUR_SENDER_ID",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.appspot.com",
+    iosBundleId: "com.example.usapp",
+  );
+}. 
