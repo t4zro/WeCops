2a10e81bfa5b1e1046c5ba63d5c51f9a1835af49
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 18:04:27 +0530

    Create auth_provider.dart

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../services/auth_service.dart';
+
+final authServiceProvider = Provider<AuthService>((ref) {
+  return AuthService();
+});
+
+final authStateProvider = StreamProvider((ref) {
+  return ref.watch(authServiceProvider).authStateChanges;
+});
