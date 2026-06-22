f7d6894123b37195a92cf85331d9f7cfc83952e1
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 19:15:13 +0530

    Create message_bubble.dart

---
+++ lib/widgets/message_bubble.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import '../models/message_model.dart';
+
+class MessageBubble extends StatelessWidget {
+  final Message message;
+  final bool isMe;
+
+  const MessageBubble({
+    super.key,
+    required this.message,
+    required this.isMe,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: isMe ? Colors.blue : Colors.grey.shade300,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Text(
+          message.text ?? '',
+          style: TextStyle(color: isMe ? Colors.white : Colors.black),
+        ),
+      ),
+    );
+  }
+}
