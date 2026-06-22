39ce753cd785d05e0ebfcb2e380b3bf5b38e15d9
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 13:59:12 +0530

    Update comment_model.dart

--- lib/models/comment_model.dart
+++ lib/models/comment_model.dart
@@ -1,1 +1,38 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Comment {
+  final String id;
+  final String userId;
+  final String userName;
+  final String text;
+  final DateTime createdAt;
+
+  Comment({
+    required this.id,
+    required this.userId,
+    required this.userName,
+    required this.text,
+    required this.createdAt,
+  });
+
+  factory Comment.fromMap(Map<String, dynamic> data) {
+    return Comment(
+      id: data['id'] ?? '',
+      userId: data['userId'] ?? '',
+      userName: data['userName'] ?? '',
+      text: data['text'] ?? '',
+      createdAt: (data['createdAt'] as Timestamp?)?.toDate() ??
+          DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'userId': userId,
+      'userName': userName,
+      'text': text,
+      'createdAt': FieldValue.serverTimestamp(),
+    };
+  }
+}
