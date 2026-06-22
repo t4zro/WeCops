9e823e221c454df130e88d2294e835a92bdde6ee
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 19:18:41 +0530

    Create app_bar_widget.dart

---
+++ lib/widgets/app_bar_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final List<Widget>? actions;
+
+  const AppBarWidget({
+    super.key,
+    required this.title,
+    this.actions,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      actions: actions,
+      centerTitle: false,
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+}
