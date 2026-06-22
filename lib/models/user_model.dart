371b074bff4a5eb77a1b2961f2f5c32940de8d1e
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 13:57:37 +0530

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,1 +1,50 @@
-. 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class AppUser {
+  final String uid;
+  final String email;
+  final String displayName;
+  final String? photoUrl;
+  final String? relationshipId;
+  final String? partnerId;
+  final bool liveWindowEnabled;
+  final DateTime? lastSeen;
+
+  AppUser({
+    required this.uid,
+    required this.email,
+    required this.displayName,
+    this.photoUrl,
+    this.relationshipId,
+    this.partnerId,
+    this.liveWindowEnabled = false,
+    this.lastSeen,
+  });
+
+  factory AppUser.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+
+    return AppUser(
+      uid: doc.id,
+      email: data['email'] ?? '',
+      displayName: data['displayName'] ?? '',
+      photoUrl: data['photoUrl'],
+      relationshipId: data['relationshipId'],
+      partnerId: data['partnerId'],
+      liveWindowEnabled: data['liveWindowEnabled'] ?? false,
+      lastSeen: (data['lastSeen'] as Timestamp?)?.toDate(),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'email': email,
+      'displayName': displayName,
+      'photoUrl': photoUrl,
+      'relationshipId': relationshipId,
+      'partnerId': partnerId,
+      'liveWindowEnabled': liveWindowEnabled,
+      'lastSeen': FieldValue.serverTimestamp(),
+    };
+  }
+}
