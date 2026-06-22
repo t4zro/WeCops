5e048f6fc9653f3bf9d50a6e9708d0da299ad46a
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 13:58:23 +0530

    Update relationship_model.dart

--- lib/models/relationship_model.dart
+++ lib/models/relationship_model.dart
@@ -1,1 +1,34 @@
-. 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RelationshipModel {
+  final String id;
+  final String code;
+  final List<String> users;
+  final DateTime? createdAt;
+
+  RelationshipModel({
+    required this.id,
+    required this.code,
+    required this.users,
+    this.createdAt,
+  });
+
+  factory RelationshipModel.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+
+    return RelationshipModel(
+      id: doc.id,
+      code: data['code'] ?? '',
+      users: List<String>.from(data['users'] ?? []),
+      createdAt: (data['createdAt'] as Timestamp?)?.toDate(),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'code': code,
+      'users': users,
+      'createdAt': FieldValue.serverTimestamp(),
+    };
+  }
+} 
