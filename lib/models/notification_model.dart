f4fa6631896bf55055f415e4846cbc45fc441ec2
t4zro <vimal.bussinessig@gmail.com>
2026-06-22 14:00:36 +0530

    Update notification_model.dart

--- lib/models/notification_model.dart
+++ lib/models/notification_model.dart
@@ -1,1 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class NotificationModel {
+  final String id;
+  final String title;
+  final String body;
+  final String type;
+  final String fromUserId;
+  final bool isRead;
+  final DateTime createdAt;
+
+  NotificationModel({
+    required this.id,
+    required this.title,
+    required this.body,
+    required this.type,
+    required this.fromUserId,
+    this.isRead = false,
+    required this.createdAt,
+  });
+
+  factory NotificationModel.fromFirestore(DocumentSnapshot doc) {
+    final data = doc.data() as Map<String, dynamic>;
+
+    return NotificationModel(
+      id: doc.id,
+      title: data['title'],
+      body: data['body'],
+      type: data['type'],
+      fromUserId: data['fromUserId'],
+      isRead: data['isRead'] ?? false,
+      createdAt: (data['createdAt'] as Timestamp).toDate(),
+    );
+  }
+
+  Map<String, dynamic> toFirestore() {
+    return {
+      'title': title,
+      'body': body,
+      'type': type,
+      'fromUserId': fromUserId,
+      'isRead': isRead,
+      'createdAt': FieldValue.serverTimestamp(),
+    };
+  }
+}
